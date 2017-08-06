fc2da2c7959127c48c9eff307ba7bca8d6d6ab8d
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-08-07 01:55:08 +0530

    Add flying plane

---
+++ assets/js/plane.js
@@ -0,0 +1,80 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function R(min, max) {
+    return min + (Math.random() * (max - min))
+};
+
+				// Create Clouds
+        var totalClouds = 10,
+            body = $('body'),
+						w = window.innerWidth,
+						h = window.innerHeight;
+
+        function createClouds(element) {
+            for (var i = 0, div; i < totalClouds; i++) {
+                cloud = document.createElement('img');
+                cloud.src = 'http://www.andrewashley.co.nz/code-examples/Plane/res/cloud-' + getRandomIntInclusive(1, 5) + '.png';
+                cloud.className = 'cloud';
+                element.append(cloud);
+                TweenMax.set(cloud, {
+                    x: R(0, w),
+                    y: R(0, h),
+                    opacity: R(0.5, 1),
+                    zIndex: R(3, 7),
+                    scale: R(0, 0.5) + 0.5
+                });
+            };
+        }
+
+        // createClouds(body);
+
+
+				// Fly
+				var pageWidth = window.innerWidth,
+            duration = pageWidth / 120,
+            d = pageWidth * 2,
+						distance = -d,
+						reverseDistance = d;
+
+        var flyingTl = new TimelineMax({
+            repeat: -1,
+            delay: 1
+        });
+        TweenMax.set('.plane-elements', { css: {'top': '4%'} });
+        TweenMax.set('.plane-elements', { scale:0.2 });
+
+
+
+        flyingTl.to('.plane-elements', duration, {
+            x: distance, y: 0, z: 0,
+            ease: Power0.easeNone, y: 0,            
+            onComplete: planeAltitude
+        })
+            .to('.plane-elements', duration, {
+                x: reverseDistance, y: 0, z: 0,
+                ease: Power0.easeNone, y: 0,
+                onComplete: planeAltitude
+            });
+
+        function planeAltitude(e) {
+            $('.plane-elements .direction').toggleClass('reverse');
+
+            var altitude = getRandomIntInclusive(5,15) + '%';
+								TweenMax.set('.plane-elements', { css: {'top': '4%'} });
+					
+					  var value = getRandomIntInclusive(2,4),
+								distanceAway = '0.' + value;
+					  
+								TweenMax.set('.plane-elements', { scale:0.2 });
+        }
+
+
+				// Propeller and Banner
+				TweenMax.to('.plane-elements', 0.9, { x:2, y:1, rotation:0.5, yoyo:true, repeat:-1 });
+				TweenMax.to('.propeller', 0.05, { rotationX:'60deg', yoyo:true,	repeat:-1 });
+				TweenMax.to('.banner', 0.7, { y:5, width:895, height: 170, yoyo:true, repeat:-1 });
+				TweenMax.to('.banner-tip', 0.65, { x:-10, y:5, height:135 , yoyo:true, repeat:-1 });
