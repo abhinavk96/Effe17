78860f80d62a048dd748900e1023484a552edd28
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-10-06 11:57:52 +0530

    Trial for scrollbar

--- events/js/grid.js
+++ events/js/grid.js
@@ -344,8 +344,10 @@ var Grid = (function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
 			this.$description = $( '<p></p>' );
+			this.$rules = $( '<div class="og-rules"></div>' );
+
 			// this.$href = $( '<a href="#">Visit website</a>' );
-			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description );
+			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$rules );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
 			this.$closePreview = $( '<span class="og-close"></span>' );
@@ -382,11 +384,14 @@ var Grid = (function() {
 					href : $itemEl.attr( 'href' ),
 					largesrc : $itemEl.data( 'largesrc' ),
 					title : $itemEl.data( 'title' ),
-					description : $itemEl.data( 'description' )
+					description : $itemEl.data( 'description' ),
+					rules: $itemEl.data('rules')
 				};
 
 			this.$title.html( eldata.title );
 			this.$description.html( eldata.description );
+			this.$rules.html( eldata.rules );
+
 			// this.$href.attr( 'href', eldata.href );
 
 			var self = this;
